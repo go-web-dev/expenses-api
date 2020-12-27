ca1b02178b4ff7c3b1fbd8240f148983f72e9bb1
steevehook <steevehook@gmail.com>
2020-12-27 17:58:48 +0200

    add dummy prometheus record metrics

--- controllers/routes.go
+++ controllers/routes.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
@@ -36,6 +40,22 @@ type RouterConfig struct {
 	AuthSvc     AuthenticationService
 }
 
+func recordMetrics() {
+	go func() {
+		for {
+			opsProcessed.Inc()
+			time.Sleep(2 * time.Second)
+		}
+	}()
+}
+
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "myapp_processed_ops_total",
+		Help: "The total number of processed events",
+	})
+)
+
 // NewRouter creates a new application HTTP router
 func NewRouter(cfg RouterConfig) http.Handler {
 	chain := alice.New(
@@ -50,8 +70,10 @@ func NewRouter(cfg RouterConfig) http.Handler {
 	routeWithBody := func(h http.Handler) http.Handler {
 		return jsonBodyChain.Then(h)
 	}
+	recordMetrics()
 
 	router := httprouter.New()
+	router.Handler(http.MethodGet, "/metrics", promhttp.Handler())
 	router.Handler(http.MethodGet, "/expenses", route(getAllExpenses(cfg.ExpensesSvc)))
 	router.Handler(http.MethodGet, "/expenses/:"+idsRouteParam, route(getExpensesByIDs(cfg.ExpensesSvc)))
 	router.Handler(http.MethodPost, "/expenses", routeWithBody(createExpense(cfg.ExpensesSvc)))
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/google/uuid v1.1.2
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/justinas/alice v1.2.0
+	github.com/prometheus/client_golang v1.9.0
 	github.com/spf13/viper v1.7.1
 	github.com/upper/db/v4 v4.0.1
 	go.uber.org/zap v1.16.0
